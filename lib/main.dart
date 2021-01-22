2707cfff284473e23907afee1227370ce49a8cee
rishika97 <61359053+rishika97@users.noreply.github.com>
2021-01-22 21:22:14 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stories/story_brain.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(Stories());
